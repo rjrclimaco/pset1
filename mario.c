a0d8c756bdf676b2d4a553cf5497478dc0a9f5a7
RJ <asdfgrjrclimaco@gmail.com>
2022-02-26 10:58:32 +0000

    Finished PSET1

--- mario.c
+++ mario.c
@@ -1,6 +1,34 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height > 8);
+    for(int i = 0; i < height; i++) // This is for row.
+    {
+        for(int j = height - i; j >1; j--)//This is for column
+        {
+             printf(" ");
+        }
+        for(int k = 0; k <= i; k++) // For the Right to Left "#"
+        {
+            printf("#");
+        }
+         printf("  ");
+         
+         for(int l = 0; l <= i; l++)//For the Left to Right ""
+         {
+              printf("#");
+         }
+            printf("\n");
 
-}
+    }
+    
+         return 0;  
+
+}  
